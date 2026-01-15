a1d341e27f1e03147bc13983cee6c637e3957bff
IMXEren <96839938+IMXEren@users.noreply.github.com>
2026-01-15 17:54:50 +0530

    fix(bug): check hash when number of rows isn't changed

--- src/check.py
+++ src/check.py
@@ -4,6 +4,7 @@ import re
 def check(row_num: int, hashed_cg: str):
     new_num = row_num
     new_hash = hashed_cg
+
     with open("data.txt", "r") as fp:
         line = fp.readline()
     extract_re = re.compile(r"(\d+):(.*)")
@@ -14,18 +15,19 @@ def check(row_num: int, hashed_cg: str):
     if matches:
         try:
             old_num = int(matches.group(1).strip())
-            old_hash = matches.group(2)
         except Exception:
             old_num = -1
 
-    if old_num == row_num:
-        return False 
-    else:
-        if new_num == -1:
-            return False
-        if new_num > old_num or (new_num == old_num or old_hash != new_hash):
-            update(f"{new_num}:{new_hash}")
-            return True
+        old_hash = matches.group(2)
+
+    if new_num in [-1, 0]:
+        return False
+    elif new_num > old_num or (new_num == old_num and new_hash != old_hash):
+        update(f"{new_num}:{new_hash}")
+        return True
+
+    return False
+
 
 def update(val: str):
     with open("data.txt", "w") as fp:
